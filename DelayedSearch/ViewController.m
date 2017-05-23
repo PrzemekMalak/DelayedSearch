ffda387420c950a428dc63883aeaacb6abf8f89e
Przemek Malak <pm@codefuture.eu>
2017-05-23 20:24:00 +0200

    Init

--- DelayedSearch/ViewController.m
+++ DelayedSearch/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UISearchBarDelegate>
+{
+    NSTimer *timer_;
+}
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
@@ -16,14 +21,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.searchBar.delegate = self;
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
+{
+    [self searchWithDelay:searchText];
 }
 
+- (void)searchWithDelay:(NSString*)searchString
+{
+    if (searchString == nil)
+    {
+        return;
+    }
+    
+    if (timer_ != nil)
+    {
+        [timer_ invalidate];
+    }
+    
+    if (searchString.length > 0)
+    {
+        NSString *settingTimerString = [NSString stringWithFormat:@"setting timer for: %@", searchString];
+        [self appendText:settingTimerString];
+        timer_ = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(invokeSearch:) userInfo:searchString repeats:NO];
+    }
+}
 
+- (void)invokeSearch:(NSTimer*)timer
+{
+    id userInfo_ = [timer userInfo];
+    
+    if (userInfo_)
+    {
+        NSString *settingTimerString = [NSString stringWithFormat:@"invoking search for: %@", timer.userInfo];
+        [self appendText:settingTimerString];
+    }
+}
+
+- (void)appendText:(NSString*)text
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSString *newString_ = [NSString stringWithFormat:@"%@\n%@ %@", self.textView.text, [NSDate date], text];
+        self.textView.text = newString_;
+    });
+}
 @end
